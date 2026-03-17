f95aaf3f6bf71e994d204369ce217136166715ea
Edward(Mac) <lankovedik@gmail.com>
2026-03-17 20:31:15 +0300

    fix: add getPathSize func

--- path_size.go
+++ path_size.go
@@ -6,6 +6,10 @@ func GetSize(path string, all bool, recursive bool) (int64, error) {
 	return cli.GetSize(path, all, recursive)
 }
 
+func GetPathSize(path string, all bool, recursive bool) (int64, error) {
+	return cli.GetSize(path, all, recursive)
+}
+
 func FormatSize(size int64, human bool) string {
 	return cli.FormatSize(size, human)
 }
