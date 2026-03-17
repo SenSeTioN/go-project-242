c0c1926f845a938736ef85c94dac1c404a4cec3a
Edward(Mac) <lankovedik@gmail.com>
2026-03-17 20:34:37 +0300

    refactor: GetPathSize

--- path_size.go
+++ path_size.go
@@ -6,8 +6,12 @@ func GetSize(path string, all bool, recursive bool) (int64, error) {
 	return cli.GetSize(path, all, recursive)
 }
 
-func GetPathSize(path string, all bool, recursive bool) (int64, error) {
-	return cli.GetSize(path, all, recursive)
+func GetPathSize(path string, human bool, all bool, recursive bool) (string, error) {
+	size, err := cli.GetSize(path, all, recursive)
+	if err != nil {
+		return "", err
+	}
+	return cli.FormatSize(size, human), nil
 }
 
 func FormatSize(size int64, human bool) string {
