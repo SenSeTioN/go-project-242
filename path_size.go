05c31efc92c565d851fe5e4aebaf2fef81475ad6
Edward(Mac) <lankovedik@gmail.com>
2026-03-17 20:27:33 +0300

    fix: add ./path_size.go

---
+++ path_size.go
@@ -0,0 +1,11 @@
+package code
+
+import "code/internal/cli"
+
+func GetSize(path string, all bool, recursive bool) (int64, error) {
+	return cli.GetSize(path, all, recursive)
+}
+
+func FormatSize(size int64, human bool) string {
+	return cli.FormatSize(size, human)
+}
